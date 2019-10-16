1707f99ba30b36301e00a2f22362a486f8f3c1ef
MaitoK <56174790+MaitoK@users.noreply.github.com>
2019-10-16 15:09:33 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-import java.util.Scanner();
+import java.util.Scanner;
 
-public Main{
-  private static void main(String args []){
+public class Main{
+  public static void main(String args []){
     System.out.println("Input 1 for scientific calculator");
     System.out.println("Input 2 for matrix calculator");
     Scanner input = new Scanner(System.in);
-    int input = input.nextInt();
-    if (x == 1){
+    int variable = input.nextInt();
+    if (variable == 1){
       //Basic calculations
       BasicCalculator bc = new BasicCalculator();
       //Scientific calculations (Incomplete)
@@ -20,13 +20,14 @@ public Main{
       System.out.println(sc.Sin(value));
       System.out.println(sc.Cos(value));
       System.out.println(sc.Tan(value));
+      System.out.println("Input value for squared and square root");
       value = input.nextInt();
       System.out.println(sc.squared(value));
       System.out.println(sc.squareRoot(value));
       System.out.println("Input value for exponent of previous value");
       int exponent = input.nextInt();
-      System.out.println(sc.exponent(value, exponent);
-    } else if (x == 2){
+      System.out.println(sc.exponent(value, exponent));
+    } else if (variable == 2){
       //Matrix calculations (Incomplete)
       Matrix m = new Matrix();
     } else {
