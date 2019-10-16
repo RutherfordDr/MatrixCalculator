ef9cf8806752a58a3c631b4bde57039b3fc4c104
MaitoK <56174790+MaitoK@users.noreply.github.com>
2019-10-15 21:14:39 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,22 @@ public Main{
       //Basic calculations
       BasicCalculator bc = new BasicCalculator();
       //Scientific calculations (Incomplete)
-      System.out.println("scientific");
+      ScientificCalculator sc = new ScientificCalculator();
+      System.out.println("Input value for log and ln");
+      int value = input.nextInt();
+      System.out.println(sc.log(value));
+      System.out.println(sc.ln(value));
+      System.out.println("Input value for sin,cos,tan");
+      value = input.nextInt();
+      System.out.println(sc.Sin(value));
+      System.out.println(sc.Cos(value));
+      System.out.println(sc.Tan(value));
+      value = input.nextInt();
+      System.out.println(sc.squared(value));
+      System.out.println(sc.squareRoot(value));
+      System.out.println("Input value for exponent of previous value");
+      int exponent = input.nextInt();
+      System.out.println(sc.exponent(value, exponent);
     } else if (x == 2){
       //Matrix calculations (Incomplete)
       Matrix m = new Matrix();
