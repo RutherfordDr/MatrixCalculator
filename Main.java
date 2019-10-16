f7c9c94ab5451df8d19994ae9f8313e445f145ca
MaitoK <56174790+MaitoK@users.noreply.github.com>
2019-10-15 20:51:02 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,17 @@
+import java.util.Scanner();
 
+public Main{
+  private static void main(String args []){
+    System.out.println("Input 1 for scientific calculator");
+    System.out.println("Input 2 for matrix calculator");
+    Scanner input = new Scanner(System.in);
+    int input = input.nextInt();
+    if (x == 1){
+      System.out.println("scientific");
+    } else if (x == 2){
+      System.out.println("matrix");
+    } else {
+      System.out.println("Out of bounds input");
+    }
+  }
+}
