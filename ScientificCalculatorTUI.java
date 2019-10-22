174253df0a7b4fada5f98136f6ed2f66d30ee4c8
gavanorio <55898037+gavanorio@users.noreply.github.com>
2019-10-22 08:59:33 -0600

    Restored files from last commit

---
+++ ScientificCalculatorTUI.java
@@ -0,0 +1,192 @@
+import java.util.Scanner;
+
+public class ScientificCalculatorTUI {
+	private ScientificCalculator scientificCalculator;
+	private boolean running;
+	private boolean writingEquation;
+	
+	/*public static void main(String[] args) {
+		
+		 * To get the ScientificCalculator running do the following:
+		 * 
+		 * 		ScientificCalculatorTUI calc = new ScientificCalculatorTUI();
+		 *		calc.start();
+		 *
+		 *
+		 * ScientificCalculatorTUI calc = new ScientificCalculatorTUI();
+		 * calc.start();
+		 *
+		}
+	*/
+	public ScientificCalculatorTUI() {
+		this.scientificCalculator = new ScientificCalculator();
+		running = true;
+		writingEquation = true;
+	}
+	
+	public void start() {
+		displayOptions();
+		while (running) {
+			promptForEquation();
+		}
+		System.out.println("Would you like to enter another equation Y/N");
+		Scanner scanner = new Scanner(System.in);
+		String procede = scanner.next();
+		if (procede.equals("Y")){
+			scientificCalculator.resetEquationString();
+			running = true;
+			writingEquation = true;
+			start();
+		} 
+		
+	}
+	
+	/*
+	 * Displays the arithmetic options in a list.
+	 * 	Ex:
+	 * 		Arithmetic Options:
+	 * 			1. log
+	 * 			2. ln
+	 * 			3. exponent
+	 */
+	public void displayOptions() {
+		String formattedOptions = "Arithmetic Options: ";
+		int number = 1;
+		for (String i: scientificCalculator.getArithmeticOptions()) {
+			formattedOptions += "\r\n\t" + number + ". " + i;
+			number += 1;
+		}
+		System.out.println(formattedOptions);
+	}
+	
+	/*
+	 * Prints out the mode/settings your Scientific Calculator is in. 
+	 *  	Radians: if radians is true that means that you're inputting angles in as radians. If false
+	 *  			 then you're inputting them as degrees.
+	 */
+	public void displayMode() {
+		if (scientificCalculator.getRadians()) {
+			System.out.println("Radians");
+		} else {
+			System.out.println("Degrees");
+		}
+	}
+	
+	public void displayEquation() {
+		System.out.println(scientificCalculator.getEquationString());
+	}
+	
+	public void displaySolve(String solution) {
+		System.out.println(solution.toString());
+	}
+	
+	public void promptForEquation() {
+		System.out.println("Enter the corresponding arithmetic option or number.");
+		Scanner scanner = new Scanner(System.in);
+		
+		if (scanner.hasNextDouble()) {
+			String input = scanner.next();
+			
+			scientificCalculator.updateEquationString(input);
+			displayEquation();
+		} else {
+		
+		String input = scanner.next();	
+		switch (input) {
+			case "=":
+				displaySolve(scientificCalculator.solve(scientificCalculator.parseEquation(scientificCalculator.getEquationString())));
+				writingEquation = false;
+				running = false;
+				break;
+			case "+":
+				scientificCalculator.updateEquationString(" + ");
+				displayEquation();
+				break;
+			case "-":
+				scientificCalculator.updateEquationString(" - ");
+				displayEquation();
+				break;
+			case "*":
+				scientificCalculator.updateEquationString(" * ");
+				displayEquation();
+				break;
+			case "/":
+				scientificCalculator.updateEquationString(" / ");
+				displayEquation();
+				break;
+			case "log":
+				scientificCalculator.updateEquationString(" log(");
+				promptForNumber();
+				displayEquation();
+				break;
+			case "ln":
+				scientificCalculator.updateEquationString(" ln(");
+				promptForNumber();
+				displayEquation();
+				break;
+			case "^2":
+				scientificCalculator.updateEquationString(" ^(2) ");
+				displayEquation();
+				break;
+			case "^":
+				scientificCalculator.updateEquationString(" ^(");
+				promptForNumber();
+				displayEquation();
+				break;
+			case "pi":
+				scientificCalculator.updateEquationString("pi");
+				displayEquation();
+				break;
+			case "(":
+				scientificCalculator.updateEquationString(" (");
+				promptForNumber();
+				displayEquation();
+				break;
+			case ")":
+				scientificCalculator.updateEquationString(" ) ");
+				displayEquation();
+				break;
+			case "sin":
+				scientificCalculator.updateEquationString(" sin(");
+				promptForNumber();
+				displayEquation();
+				break;
+			case "cos":
+				scientificCalculator.updateEquationString(" cos(");
+				promptForNumber();
+				displayEquation();
+				break;
+			case "tan":
+				scientificCalculator.updateEquationString(" tan(");
+				promptForNumber();
+				displayEquation();
+				break;
+			default:
+				System.out.println("Not an option.");
+				displayOptions();
+				promptForEquation();
+			}
+		}
+	}
+
+	public void promptForNumber() {
+		System.out.println("Enter a number.");
+		Scanner scanner = new Scanner(System.in);
+		if (scanner.hasNextDouble()) {
+			String number = scanner.next();
+			scientificCalculator.updateEquationString(" " + number);
+		} else {
+			String pi = scanner.next();
+			if (pi.equals("pi")) {
+				scientificCalculator.updateEquationString(pi);
+			} else {
+				promptForNumber();
+			}
+		}
+		
+	}
+	
+	
+	
+	
+}
