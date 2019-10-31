042b1621994021c0043607e09e24c8022e7b7f62
gavanorio <55898037+gavanorio@users.noreply.github.com>
2019-10-31 14:15:19 -0600

    Add files via upload

---
+++ BasicCalculatorGUI.java
@@ -0,0 +1,47 @@
+public class BasicCalculatorGUI extends BasicCalculator{
+
+	private BasicCalculator BasicCalculator;
+	private String operation = "";
+	public String temp;
+	
+	public BasicCalculatorGUI() {
+		this.BasicCalculator = new BasicCalculator();
+		start();
+	}
+	
+	public void start() {
+		
+		
+	}
+	
+	public void addNumberToOp() {
+		operation += temp;
+	}
+	
+	public String getOperation() {
+		return operation;
+	}
+	
+	public void Operation(String op) {
+		
+		if (op == "CLEAR") {
+			operation = "";
+		}
+		else {
+			String ops = BasicCalculator.BasicOperations(op);
+			if (ops == "notvalid") {
+				System.out.print("Last input errased. Enter a valid operation");
+			}
+			else {
+				operation += ops;
+			}
+		}
+
+	}
+	
+	public double equals() {
+		computeResult();
+		return result;
+		
+	}
+}
